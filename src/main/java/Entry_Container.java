dc03610f95fb1c8ed033ccd6ed0af0230510ab62
orloveste <orlovevm@yahoo.com>
2020-05-28 17:33:20 +0300

    Java - Class Entry - test Astraction
    https://github.com/users/orloveste/projects/5#card-39082671

---
+++ src/main/java/Entry_Container.java
@@ -0,0 +1,18 @@
+public abstract class Entry_Container {
+    //Schema SQL  = Entry
+    protected int idAccept_DGPR;
+    protected boolean accept_DGPR;
+
+    public Entry_Container(int idAccept_DGPR, boolean accept_DGPR) {
+        this.idAccept_DGPR = idAccept_DGPR;
+        this.accept_DGPR = accept_DGPR;
+    }
+
+    public int getIdAccept_DGPR() {
+        return idAccept_DGPR;
+    }
+
+    public boolean isAccept_DGPR() {
+        return accept_DGPR;
+    }
+}
